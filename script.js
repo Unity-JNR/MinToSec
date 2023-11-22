83d912d94da3d83eba02f35b76939415331dd8b0
Unity-JNR <unitydemas12@gmail.com>
2023-11-22 10:19:22 +0200

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let btnConvert = document.querySelector('[data-convert]');
-let seconds = document.getElementById('Sec'); 
-let min = document.getElementById('Min').value
+let btnConvert = document.querySelector('[data-convert]');// this allows you to use the button
+let seconds = document.getElementById('Sec'); // this displays to the input seconds
+let min = document.getElementById('Min').value // this fetches data inputted into mintues
 
 
-
+// this makes the button function when clicked
 btnConvert.addEventListener('click', function() {
     let min = document.getElementById('Min').value
 
    minToSec(min);
 
 })
-
+// this does the sum to make that minutes converts to seconds
 function minToSec(num) {
     let sum = num * 60 ;
 seconds.value = sum
